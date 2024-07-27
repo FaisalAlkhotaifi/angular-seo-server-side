8fcab56bcf13857424d3ec8008eed87bdea913eb
Faisal Alkhotaifi <111806787+falkhotaifidev@users.noreply.github.com>
2024-07-27 14:56:06 +0300

    seo with ssr #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { MetaService } from './services/meta.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-seo-server-side';
+  
+  constructor(
+    private _router: Router,
+    private _metaService: MetaService,
+  ){}
+
+  ngOnInit(): void {
+    this._router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((x) => x.urlAfterRedirects),
+      tap((data: string) => this._metaService.updateMetaTags(data))
+    ).subscribe()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
